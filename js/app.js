b8790f690bfd10d5455cb63674c5b1f503ce68ca
jakebillings <jake@jakebillings.com>
2018-09-16 09:25:55 -0400

    Updates

--- js/app.js
+++ js/app.js
@@ -1,40 +1,14 @@
 // Default Google Map Options
-
-
-if (Object.is(localStorage.disasterMapOptions, undefined)) {
-    localStorage.disasterMapOptions = '{}'
-}
-
-disasterMapOptions = JSON.parse(localStorage.disasterMapOptions);
-
-function saveOptions() {
-    localStorage.disasterMapOptions = JSON.stringify(disasterMapOptions);
-}
-
-if (Object.entries(disasterMapOptions).length === 0) {
-    disasterMapOptions = {
-        zoom: 15,
-        center: {
-            lat: 42.7284,
-            lng: -73.6918
-        }
+var myOptions = {
+    zoom: 15,
+    center: {
+        lat: 42.7284,
+        lng: -73.6918
     }
-    saveOptions()
-}
-
+};
 
 // Create a map object using the Google Maps API
-var map = new google.maps.Map(document.getElementById("map-canvas"), disasterMapOptions);
-
-map.addListener('center_changed', function() {
-    disasterMapOptions.center = map.getCenter();
-    saveOptions()
-});
-
-map.addListener('zoom_changed', function() {
-    disasterMapOptions.zoom = map.getZoom();
-    saveOptions()
-});
+var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
 
 // Create the Gmaps Heatmap Layer
 var heatmap = new HeatmapOverlay(map,
@@ -53,50 +27,83 @@ var heatmap = new HeatmapOverlay(map,
     }
 );
 
-//Make an asynchronous call to load data about the victims
-$.getJSON('https://openwhisk.ng.bluemix.net/api/v1/web/Disaster-Assist_dev/default/disaster-map-backend.json', {}, function (res) {
-    //Set the dataset for the heatmap to the result of the call to the backend
-    heatmap.setData({
-        max: 8,
-        data: res.body
+//==== Heat Map Data ====
+function reloadHeatmapData() {
+    //Make an asynchronous call to load data about the victims
+    $.getJSON('https://openwhisk.ng.bluemix.net/api/v1/web/Disaster-Assist_dev/default/disaster-map-backend.json', {}, function (res) {
+        //Set the dataset for the heatmap to the result of the call to the backend
+        heatmap.setData({
+            max: 8,
+            data: res.body
+        });
+
+        //Calculate the average latitude and longitude so that we can center the map
+        // on an area that matters
+        // var avgs = res.body.reduce(function (accumulator, datum) {
+        //     return {
+        //         lat: accumulator.lat + datum.lat / res.body.length,
+        //         lng: accumulator.lng + datum.lng / res.body.length
+        //     };
+        // }, {
+        //     lat: 0,
+        //     lng: 0
+        // });
+
+        //Center the map on the average latitute and longitude
+        // map.setCenter(avgs);
     });
-
-    //Calculate the average latitude and longitude so that we can center the map
-    // on an area that matters
-    // var avgs = res.body.reduce(function (accumulator, datum) {
-    //     return {
-    //         lat: accumulator.lat + datum.lat / res.body.length,
-    //         lng: accumulator.lng + datum.lng / res.body.length
-    //     };
-    // }, {
-    //     lat: 0,
-    //     lng: 0
-    // });
-
-    //Center the map on the average latitute and longitude
-    // map.setCenter(avgs);
-});
+}
+reloadHeatmapData();
 
 
 //==== Cluster Markers ====
 var markers = [];
+
 function reloadMarkers() {
-    markers.forEach(function (marker) {
-        marker.setMap(null);
-    });
-    markers = [];
 
     $.getJSON('https://openwhisk.ng.bluemix.net/api/v1/web/Disaster-Assist_dev/default/disaster-clustering.json', {}, function (res) {
-        res.clusters.forEach(cluster => {
-            markers.push(new google.maps.Marker({
-                position: {
-                    lat: cluster.location.latitude,
-                    lng: cluster.location.longitude
-                },
-                map: map,
-                title: 'Event'
-            }));
-        });
+        if (res.clusters.length === markers.length) {
+            markers.forEach(function (marker, i) {
+                marker.setPosition({
+                    lat: res.clusters[i].location.latitude,
+                    lng: res.clusters[i].location.longitude
+                });
+            });
+        } else {
+            markers.forEach(function (marker) {
+                marker.setMap(null);
+            });
+            markers = [];
+
+            res.clusters.forEach(cluster => {
+                markers.push(new google.maps.Marker({
+                    position: {
+                        lat: cluster.location.latitude,
+                        lng: cluster.location.longitude
+                    },
+                    map: map,
+                    title: 'Event'
+                }));
+            });
+        }
     });
 }
+
 reloadMarkers();
+
+function reloadEverything() {
+    reloadMarkers();
+    reloadHeatmapData();
+}
+
+//Autmatic Polling
+function automaticReloadLoop() {
+    reloadEverything();
+    setTimeout(automaticReloadLoop, 1000);
+}
+
+if (window.location.hash === '#reload') {
+    automaticReloadLoop();
+} else {
+    reloadEverything();
+}
